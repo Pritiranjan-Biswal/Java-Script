0f65c0e7e09ac11fee1e8db87f37121a0b2077fa
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-02 14:38:16 +0000

    console.log(Number("20") + "10");

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -32,6 +32,9 @@ console.log(str3);
 // let a = "10";
 // let b = 5;
 // console.log(a - b);
-console.log("5" + 2 + 3);
+// console.log("5" + 2 + 3);
 
-console.log(2 + 3 + "5");
+// console.log(2 + 3 + "5");
+console.log(Number("20") + "10");
+
+
