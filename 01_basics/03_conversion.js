80318cde8c7f47d535b46dd49d30a033097ed3b2
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-02 14:34:27 +0000

    ok

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -11,10 +11,21 @@ console.log(numberInvalue)
 
 let score=33
 console.log(typeof score);
-*/
+
 
 
 let str1="kuna"
 let str2="biswal"
  let str3=str1+str2
 console.log(str3);
+
+*/
+
+// console.log("1"+2+2)
+
+// console.log("1"+2+"3");
+
+let a = "10";
+let b = 5;
+console.log(a + b);
+
