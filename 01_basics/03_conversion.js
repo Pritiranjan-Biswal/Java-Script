35224a681addac78f1588ed7d54d50aca45cab2b
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-02 14:04:34 +0000

    bo

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,3 +1,4 @@
+/*
 let value="33fjbsjfjs"
 console.log(typeof value)
 
@@ -5,3 +6,9 @@ console.log(typeof value)
 let numberInvalue=Number(value)
 console.log(typeof numberInvalue)
 console.log(numberInvalue)
+
+
+
+let score=33
+console.log(typeof score);
+*/
