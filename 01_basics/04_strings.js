faa9fc8746252194030673671cfe27c89f9b0edb
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-06 14:26:44 +0000

    console.log(str.slice(-3));

--- 01_basics/04_strings.js
+++ 01_basics/04_strings.js
@@ -1,2 +1,43 @@
 let str="kuna";
-console.log(str.length);
+// console.log(str.length);
+
+// console.log(str.at(1));
+// console.log(str.at(4));
+// console.log(str.at(3));
+
+// console.log(str.at(-2));
+
+console.log(str.slice(0,4));
+console.log(str.slice(-3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
