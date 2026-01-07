68f920ccadb3492ec792f536da6763a62efa1a1d
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-07 15:58:58 +0000

    console.log((Math.random()*10)+1);

--- 01_basics/05_nums.js
+++ 01_basics/05_nums.js
@@ -21,3 +21,9 @@ console.log(Math.ceil(4.2));
 console.log(Math.floor(4.9));
 console.log(Math.min(2,3,4));
 console.log(Math.max(2,3,4));
+
+console.log(Math.random());
+
+console.log((Math.random()*10)+1);
+console.log((Math.floor(Math.random()*10)+1);
+
