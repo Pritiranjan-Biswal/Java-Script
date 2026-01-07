57282a959eaa52f4e7c084710d7100437614cee3
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-07 16:42:42 +0000

    // Dates
    let myDate = new Date()
    // console.log(myDate.toString());
    // console.log(myDate.toDateString());
    // console.log(myDate.toLocaleString());
    // console.log(typeof myDate);
    
    // let myCreatedDate = new Date(2023, 0, 23)
    // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
    // let myCreatedDate = new Date("2023-01-14")
    let myCreatedDate = new Date("01-14-2023")
    // console.log(myCreatedDate.toLocaleString());
    
    let myTimeStamp = Date.now()
    
    // console.log(myTimeStamp);
    // console.log(myCreatedDate.getTime());
    // console.log(Math.floor(Date.now()/1000));
    
    let newDate = new Date()
    console.log(newDate);
    console.log(newDate.getMonth() + 1);
    console.log(newDate.getDay());
    
    // `${newDate.getDay()} and the time `
    
    newDate.toLocaleString('default', {
        weekday: "long",
    
    })

--- 01_basics/06_dates.js
+++ 01_basics/06_dates.js
@@ -1,2 +1,8 @@
-let myDate=new Date();
+// let myDate=new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+
+let myDate=new Date(2026,0,7);
 console.log(myDate);
