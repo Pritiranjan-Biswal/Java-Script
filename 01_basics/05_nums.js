4c3e279df8c0f491830495ea0b392e7f94baba91
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-07 16:00:51 +0000

    Math.floor(Math.random() * (max - min + 1)) + min

--- 01_basics/05_nums.js
+++ 01_basics/05_nums.js
@@ -25,5 +25,8 @@ console.log(Math.max(2,3,4));
 console.log(Math.random());
 
 console.log((Math.random()*10)+1);
-console.log((Math.floor(Math.random()*10)+1);
+console.log((Math.floor(Math.random()*10)+1));
 
+const min = 10;
+const max = 20;
+Math.floor(Math.random() * (max - min + 1)) + min
