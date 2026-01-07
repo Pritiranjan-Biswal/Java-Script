ebf3e807e955b589d30df1ed89c8b55ea961e7a5
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-07 14:18:42 +0000

    console.log(Math); console.log(Math.abs(-4)); console.log(Math.round(4.6)); console.log(Math.ceil(4.2)); console.log(Math.floor(4.9)); console.log(Math.min(4, 3, 6, 8)); console.log(Math.max(4, 3, 6, 8));
    console.log(Math.random());
    console.log((Math.random()*10) + 1);
    console.log(Math.floor(Math.random()*10) + 1);
    
    const min = 10
    const max = 20
    
    console.log(Math.floor(Math.random() * (max - min + 1)) + min)

--- 01_basics/05_nums.js
+++ 01_basics/05_nums.js
@@ -18,3 +18,6 @@ console.log(Math.abs(-4));
 console.log(Math.round(3.5));
 console.log(Math.ceil(4.5));
 console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(2,3,4));
+console.log(Math.max(2,3,4));
