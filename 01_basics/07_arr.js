ca514b0c0b85b4bac1cf4c2c94b327c3130c429d
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-08 16:51:21 +0000

    myArr.unshift(9)

--- 01_basics/07_arr.js
+++ 01_basics/07_arr.js
@@ -1,3 +1,8 @@
-let arr = [1,2,3];
-arr.push(4)
-console.log(arr);
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[1]);
+
+myArr.pop()
+console.log(myArr)
