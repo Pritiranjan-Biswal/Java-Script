26d8204b3b5b57cc56d2ce785ff8a8b46a5b90ed
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-11 16:25:22 +0000

    c

--- 01_basics/objects.js
+++ 01_basics/objects.js
@@ -1,20 +1,20 @@
 
-const mysyb=Symbol("key1");
-const  Juser= {
-    "full name":"kuna",
-    age:18
-    [mysyb]="priti",
-    location:"Hyderabad",
-    isLoggedin:false,
-    email:"kuna@gmail.com"
+// const mysyb=Symbol("key1");
+// const  Juser= {
+//     "full name":"kuna",
+//     age:18
+//     [mysyb]="priti",
+//     location:"Hyderabad",
+//     isLoggedin:false,
+//     email:"kuna@gmail.com"
 
-}
+// }
 
-Juser.email="priti@google.com";
+// Juser.email="priti@google.com";
 
-console.log(Juser["email"]);
+// console.log(Juser["email"]);
 
-Object.isFrozen(Juser); // true
+// Object.isFrozen(Juser); // true
 
-Object.isFrozen(Juser); 
-console.log(Object.isFrozen(Juser));
+// Object.isFrozen(Juser); 
+// console.log(Object.isFrozen(Juser));
