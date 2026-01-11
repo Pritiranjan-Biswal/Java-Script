06b1a7b4c482ef7a148c96de3c2c98b14b973e41
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-11 11:02:27 +0000

    n

--- 01_basics/objects.js
+++ 01_basics/objects.js
@@ -3,7 +3,10 @@ const mysyb=Symbol("key1");
 const  Juser= {
     name:"kuna",
     age:18
-    [mysyb]="priti"
+    [mysyb]="priti",
+    location:"Hyderabad",
+    isLoggedin:false
+
 }
 
-console.log(typeof mysyb);
+console.log(Juser);
