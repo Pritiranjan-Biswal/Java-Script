7cef6583ce4804ce140bb88d05be9268e7210137
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-12 14:17:06 +0000

    b

--- 01_basics/objects.js
+++ 01_basics/objects.js
@@ -30,14 +30,34 @@
 // console.log(User);
 
 
-const mine ={
-     fullname : {
-        userfullname :{
-            firstname : "kuna",
-            lastname:"biswal"
-        }
-     }
+// const mine ={
+//      fullname : {
+//         userfullname :{
+//             firstname : "kuna",
+//             lastname:"biswal"
+//         }
+//      }
 
-}
+// }
 
-console.log(mine.fullname.userfullname.firstname);
+// console.log(mine.fullname.userfullname.firstname);
+
+
+// const num1={1:"a", 2:"b"}
+// const num2 ={3:"c",4:"d"}
+
+// const num3=Object.assign({}, num1,num2);
+// console.log(num3);
+
+
+const User={}
+
+User.id="123abc"
+User.name="kuna"
+User.sub="Math"
+
+
+console.log(Object.keys(User));
+console.log(Object.values(User));
+console.log(Object.entries(User));
+console.log(User.hasOwnProperty("sub"));
