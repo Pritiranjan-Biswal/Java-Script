bea95d47b3f6bb02fda0aae77b25ff961ce74f56
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-17 10:20:49 +0000

    def

--- practice.js
+++ practice.js
@@ -9,7 +9,9 @@ let str="   KUNA";
 // console.log(str.substring(0,4));
 
 
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-console.log(str.trim());
-console.log(str.includes("KU"));
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.trim());
+// console.log(str.includes("KU"));
+
+
