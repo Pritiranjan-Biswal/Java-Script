60e664db5af7e5a97560dd88863fcbe81a91f235
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-25 19:40:44 +0530

    flow

---
+++ control_flow.js
@@ -0,0 +1,14 @@
+const score=300;
+
+if(score>200){
+
+    const able="fly"
+    console.log(`You have Passed the exam and yor are allowed to ${able}`);
+    
+}
+else {
+    console.log("you have not passed th e exam");
+    
+}
+
+console.log('your result has been declared ');
