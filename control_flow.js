feb64df0931044d927623852ad1c2ba62f763c25
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-26 19:41:19 +0530

    iterator

--- control_flow.js
+++ control_flow.js
@@ -1,14 +1,41 @@
-const score=300;
+// const score=300;
 
-if(score>200){
+// if(score>200){
 
-    const able="fly"
-    console.log(`You have Passed the exam and yor are allowed to ${able}`);
+//     const able="fly"
+//     console.log(`You have Passed the exam and you are allowed to ${able}`);
     
-}
-else {
-    console.log("you have not passed th e exam");
+// }
+// else {
+//     console.log("you have not passed th e exam");
     
-}
+// }
 
-console.log('your result has been declared ');
+// console.log('your result has been declared ');
+
+
+
+// const month=3;
+
+// switch(month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+
+//     default:
+//         console.log("the month is invalid");
+        
+
+        
+// }
+
+let val1;
+val1= undefined ?? 10;
+console.log(val1);
+
---
+++ iterations.js
@@ -0,0 +1,6 @@
+for (let kuna = 0; kuna < 10; kuna++) {
+    const element = kuna;
+    console.log(element);
+    
+    
+}
