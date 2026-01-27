2965a149a789af26d1f11eb302eb33d0933e95f0
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-27 18:55:46 +0530

    fm

--- iterations.js
+++ iterations.js
@@ -1,8 +1,11 @@
-for (let mine = 0; mine < 10; mine++) {
-    const element = mine;
-    console.log(element);
+// for (let mine = 0; mine < 10; mine++) {
+//     const element = mine;
+//     console.log(element); 
+// }
 
 
-    
+const arr=[1, 2, 3, 4, 5];
+for(const num of arr) {
+    console.log(num);
     
 }
