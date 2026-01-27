c30a09b1d240e2de4cda8876dc7cb234aa65338a
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-27 20:06:27 +0530

    finish

--- iterators.js
+++ iterators.js
@@ -1,29 +1,68 @@
-const greetings="Hello World";
-for(const greet of greetings) {
-    // console.log(`Each char is  ${greet}`);
+// const greetings="Hello World";
+// for(const greet of greetings) {
+//     // console.log(`Each char is  ${greet}`);
     
-}
+// }
 
-const map= new Map()
-map.set('IN', "India");
-map.set('PAK', "Pakistan");
-map.set('SL', "Sri Lanka");
+// const map= new Map()
+// map.set('IN', "India");
+// map.set('PAK', "Pakistan");
+// map.set('SL', "Sri Lanka");
 
-//console.log(map);
+// //console.log(map);
 
-for(const [key, value] of map) {
-    // console.log(key, ':-',value);
+// for(const key in map) {
+//     console.log(key);
     
-}
+// }
 
-const myObject ={
-    game1:"PUBG",
-    game2:"Free Fire"
-}
-console.log(myObject);
+// const myObject ={
+//     game1:"PUBG",
+//     game2:"Free Fire"
+// }
+// // console.log(myObject);
 
 
-for (const [key, value] of myObject) {
-    console.log(key, value);
+// for (const [key, value] of myObject) {
+    // console.log(key, value);
     
-}
+// }
+
+// const kObject= {
+//     name1:"kuna",
+//     name2:"muna",
+//     name3:"suman",
+//     name4:"lipi"
+    
+    
+// }
+
+
+// for(const key in kObject) {
+//     console.log(kObject[key]);
+    
+// }
+
+
+const myCodig=[
+    {
+        languageName:"JavaScript",
+        languageFileName:"js"
+    },
+    {
+        languageName:"python",
+        languageFileName:"py"
+    },
+    {
+        languageName:"c++",
+        languageFileName:"cpp"
+    }
+]
+
+myCodig.forEach((item)=>{
+    console.log(item.languageName);
+    
+})
+
+
+
