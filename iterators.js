3198ec96c611c540854abdee12d07ac4e3973fe3
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-27 19:24:55 +0530

    wjwb

--- iterators.js
+++ iterators.js
@@ -9,5 +9,21 @@ map.set('IN', "India");
 map.set('PAK', "Pakistan");
 map.set('SL', "Sri Lanka");
 
+//console.log(map);
 
-console.log(map);
+for(const [key, value] of map) {
+    // console.log(key, ':-',value);
+    
+}
+
+const myObject ={
+    game1:"PUBG",
+    game2:"Free Fire"
+}
+console.log(myObject);
+
+
+for (const [key, value] of myObject) {
+    console.log(key, value);
+    
+}
