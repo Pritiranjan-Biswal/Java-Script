e2cbc0d06929dd49c8334ff8e22a0713d8ccd70e
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-27 19:10:17 +0530

    jhjn

---
+++ iterators.js
@@ -0,0 +1,13 @@
+const greetings="Hello World";
+for(const greet of greetings) {
+    // console.log(`Each char is  ${greet}`);
+    
+}
+
+const map= new Map()
+map.set('IN', "India");
+map.set('PAK', "Pakistan");
+map.set('SL', "Sri Lanka");
+
+
+console.log(map);
