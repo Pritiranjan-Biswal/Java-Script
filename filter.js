c9a20884b2d00d7011456cf79768d5f22313a63e
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-29 14:14:04 +0530

    fruits

--- filter.js
+++ filter.js
@@ -1,4 +1,12 @@
-const nums=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const newNumb=nums.filter((numbs)=> numbs>4)
-console.log(newNumb);
+// const nums=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const newNumb=nums.filter((numbs)=> numbs>4)
+// console.log(newNumb);
+
+
+const fruits=['Apple', 'Orange', "Grapes", 'Banana'];
+
+
+fruits.forEach((fruit)=>{
+    console.log(fruit);
     
+})
