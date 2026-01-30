18c6528b55e8db250356472114d6b95e0da02521
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-30 09:43:12 +0530

    filter

--- map.js
+++ map.js
@@ -1,4 +1,10 @@
-const arr=[1,2,3,4];
+// const arr=[1,2,3,4];
 
-const double_arr=arr.map((item)=>item*2);
-console.log(double_arr);
+// const double_arr=arr.map((item)=>item*2);
+// // console.log(double_arr);
+
+
+const arr=[1,2,3,4,5,6,7,8,9];
+const cal_even_arr=arr.filter((item)=> item%2==0);
+console.log(cal_even_arr);
+
