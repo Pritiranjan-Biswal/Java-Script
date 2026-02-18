448a764a05455f22b3821dffab7dfb0042c987e9
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-02-18 10:04:07 +0530

    jkjsjk

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,18 +1,25 @@
-console.log("Kuna")
-var val = 10;
+// console.log("Kuna")
+// var val = 10;
 
-console.log(val);//10
+// console.log(val);//10
 
-console.log(3+2);
+// console.log(3+2);
 
-console.log(typeof 4)
+// console.log(typeof 4)
 
-console.log(typeof "kuna")
+// console.log(typeof "kuna")
 
-console.log(typeof null)
+// console.log(typeof null)
 
-console.log(typeof undefined)
+// console.log(typeof undefined)
 
 
-
+function add(a, b) {
+    console.log(a+b);
+    console.log(a-b);
+    console.log(a*b);
+    console.log(a/b);
+    console.log(a%b);   
+}
+add(2,3);
 
