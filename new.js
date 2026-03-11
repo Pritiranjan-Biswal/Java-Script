4da8db9034e26baa99bb3d12f010a01b54eae34c
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-03-11 09:59:51 +0530

    ejfkhf

--- new.js
+++ new.js
@@ -1,19 +1,26 @@
-const p=document.getElementsByTagName('h1');
+// const p=document.getElementsByTagName('h1');
 
-for(let i=0; i<p.length; i++) {
-    if(i%2 != 0) {
-        p[i].innerText="I am a Student";
-        p[i].style.backgroundColor="green"
+// for(let i=0; i<p.length; i++) {
+//     if(i%2 != 0) {
+//         p[i].innerText="I am a Student";
+//         p[i].style.backgroundColor="green"
         
-    }
-}
+//     }
+// }
 
-console.log("hello world");
-console.log("hello world");
-console.log("hello kun");
-console.log("hello world");
-console.log("hello world");
-console.log("hello kun");
-console.log("hello world");
-console.log("hello world");
-console.log("hello kun");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello kun");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello kun");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello kun");
+
+
+
+let a= document.querySelector('h1');
+a.style.color="red";
+a.style.backgroundColor="pink";
+//console.dir(a);
