18d0ae577ade525a4ea305a7de677a78ea8beda0
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-03-16 23:05:59 +0530

    kkf

--- Practice/day2.js
+++ Practice/day2.js
@@ -1,9 +1,12 @@
 let str="kuna";
 // console.log(str.length);
-console.log(str.at(4));
+// console.log(str.at(4));
 
-console.log(str.charAt(4));
+// console.log(str.charAt(4));
 
+console.log(str.slice(0, -2));
+
+console.log(str.substring(0,3));
 
 
 
