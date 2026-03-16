bede472286bbd0b6111067c5d3a6f08fe4468677
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-03-16 23:18:35 +0530

    kkf

--- Practice/day2.js
+++ Practice/day2.js
@@ -1,16 +1,26 @@
-let str="kuna";
+let str1="akqquna";
+let str2="BISWAL"
 // console.log(str.length);
 // console.log(str.at(4));
 
 // console.log(str.charAt(4));
 
-console.log(str.slice(0, -2));
+// console.log(str.slice(0, -2));
 
-console.log(str.substring(0,3));
+// console.log(str.substring(0,3));
 
-
-
-
+// console.log(str1.toUpperCase());
 
 
+// console.log(str2.toLowerCase());
 
+
+// console.log(str1.trim());
+
+// console.log(str1.includes("k"));
+
+// console.log(str1.indexOf('a'));
+
+// console.log(str1.lastIndexOf('a'));
+
+console.log(str1.replaceAll('a', 'p'));
