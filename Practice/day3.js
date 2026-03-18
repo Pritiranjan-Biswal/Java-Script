31fae46b8eed8b5529b840e70f38b9c3ed3d70e7
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-03-18 14:27:04 +0530

    k

--- Practice/day3.js
+++ Practice/day3.js
@@ -5,5 +5,13 @@ const myArr=[1,2,3,4];
 // myArr.push(7);
 // myArr.pop()
 // myArr.unshift(-1)
-myArr.shift()
-console.log(myArr);
+// myArr.shift()
+// console.log(myArr);
+// console.log(myArr.includes(6));
+
+// console.log(myArr.indexOf(3));
+
+
+// const newArr=myArr.join()
+// console.log(newArr);
+// console.log(typeof(newArr));
