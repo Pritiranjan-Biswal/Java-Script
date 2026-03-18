c7a7aa60d431c719aa91ae6dc6d9d53132369e25
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-03-18 14:37:24 +0530

    spread operator

--- Practice/day3.js
+++ Practice/day3.js
@@ -15,3 +15,15 @@ const myArr=[1,2,3,4];
 // const newArr=myArr.join()
 // console.log(newArr);
 // console.log(typeof(newArr));
+// let newArr=myArr.splice(0,2);
+// console.log(newArr);
+// console.log(myArr);
+
+const marvel_heros=["thor", "iron man", "spiderman"]
+const dc_heros=["superman", "flash", "batman"]
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+
+
+const all_heros=[...marvel_heros,...dc_heros]
+console.log(all_heros);
