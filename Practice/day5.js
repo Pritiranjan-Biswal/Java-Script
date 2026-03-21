11db0a1efe8ee0c2b347bb4698a525db2298ffb7
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-03-21 19:54:12 +0530

    practicing functions

---
+++ Practice/day5.js
@@ -0,0 +1,20 @@
+// function practice() {
+//     let a=5;
+//     console.log(a);
+    
+// }
+
+// practice()
+// console.log(a);
+
+
+var a="kuna"
+
+function practice() {
+    let a="lipi"
+    console.log("Hi "+a);
+    
+    
+}
+console.log("Hi "+a);
+practice()
