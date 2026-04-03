a65c4930fd9740012de4a9158afe3fe5d793344a
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-04-03 15:39:55 +0530

    map, filter, reduce

--- Practice/day6.js
+++ Practice/day6.js
@@ -15,11 +15,12 @@
 // console.log(new_arr);
 
 
-const numbers=[1,2,3,4]
-const sum=numbers.reduce(function(result, item) {
-    result+=item;
-    return result;
-}, 0);
+// const numbers=[1,2,3,4]
+// const sum=numbers.reduce(function(result, item) {
+//     result+=item;
+//     return result;
+// }, 0);
 
-console.log(sum);
+// console.log(sum);
+
 
---
+++ Practice/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+const numbers=[1,2,3,4]
+// const sum=numbers.reduce(function(result, item) {
+//     result+=item;
+//     return result;
+// }, 0);
+
+// console.log(sum);
