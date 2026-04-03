e9cd4644d31a10636de579924e33b5d40875fb22
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-04-03 15:30:15 +0530

    map, filter

--- Practice/day6.js
+++ Practice/day6.js
@@ -1,7 +1,16 @@
-    arr=[1,2,3,4]
-    const double =arr.map((item) => {
-        return item*2;
-        // console.log(item);
+    // arr=[1,2,3,4]
+    // const double =arr.map((item) => {
+    //     return item*2;
+    //     // console.log(item);
         
-    })
-    console.log(double);
+    // })
+    // console.log(double);
+
+
+let   arr=[1,2,3,4]
+let new_arr=arr.filter((item) => {
+    if(item%2==0)
+    return item;
+})
+console.log(new_arr);
+
