af23ee5a39c9dbef9d6f0dfd925c7009428c29c7
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-04-03 15:46:01 +0530

    m

---
+++ Interview_Youtube/day1.js
@@ -0,0 +1,15 @@
+// let abc="Nausad"
+// abc[2]="r"
+// console.log(abc);
+//Is it changed or not ?
+//Ans:- NO, it can't be changed, because Because strings in JavaScript are immutable.
+
+
+//----------------------------
+
+let a=100
+let z=++a
+console.log(a+z);
+
+//in pre-increament ++a=101 & a++=100
+
